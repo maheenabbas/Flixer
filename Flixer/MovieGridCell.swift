4c124829a0b87828476c4820eeaabe2d04571ee3
Maheen Naqvi <maheenabbas99a@gmail.com>
2021-09-20 18:22:00 -0400

    IOS APP

---
+++ Flixer/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixer
+//
+//  Created by Maheen Naqvi on 9/20/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
